ddc1b758de5f5c1130407e9ea7f32254d25fd4a3
roberth303z <167308980+roberth303z@users.noreply.github.com>
2024-04-17 07:55:20 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner=new Scanner (System.in);
+    System.out.print("bem-vindo á calculadora simples!\n");
+
+    System.out.print("numero 1: ");
+    int num1= scanner.nextInt();
+    System.out.print("numero 2: ");
+    int num2= scanner.nextInt();
+    System.out.print("operacao (+,-,*,/): ");
+    char operacao = scanner.next().charAt(0);
+
+     if (operacao== '+'){
+      System.out.println("o resultado é: " + soma(num1, num2));
+    }else if (operacao== '-'){
+      System.out.println("o resultado é: " + subtracao(num1, num2));
+    }else if (operacao== '*'){
+      System.out.println("o resultado é: " + multiplicacao(num1, num2));
+    }else if (operacao== '/'){
+      if (num2 !=0){
+        System.out.println("o resultado é: " + divisao(num1, num2));
+      } else {
+        System.out.println("erro: divisao por zero!");
+        return;
+      }
+    } else {
+      System.out.println("operacao invalida!"); 
+    }
+  }
+  public static int soma(int a, int b){
+    return a+b;
+  }
+  public static int subtracao(int a, int b){
+    return a-b;
+  }
+  public static int multiplicacao(int a, int b){
+    return a*b;
+  }
+  public static int divisao(int a, int b){
+    return a/b;
+  }
+}
